b8918ab65bbf785f5fccd8207578f63ffe84f27d
AndersonZhangyq <andersonzhangyq@gmail.com>
2024-05-26 09:59:45 +0800

    add plus substract support

--- lib/widget/moneyNumberTablet.dart
+++ lib/widget/moneyNumberTablet.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class MoneyNumberTablet extends StatelessWidget {
-  const MoneyNumberTablet(
+class MoneyNumberTablet extends StatefulWidget {
+  MoneyNumberTablet(
       {Key? key, required this.moneyController, required this.callback})
       : super(key: key);
 
@@ -9,10 +9,68 @@ class MoneyNumberTablet extends StatelessWidget {
 
   final void Function(String text)? callback;
 
+  @override
+  State<MoneyNumberTablet> createState() => _MoneyNumberTabletState();
+}
+
+class _MoneyNumberTabletState extends State<MoneyNumberTablet> {
+  int symbolCounter = 0;
+
   void _onNumberTabletPressed(
-      {int? number, bool isDot = false, bool isRemove = false}) {
-    String ret = moneyController.text;
+      {int? number,
+      bool isDot = false,
+      bool isRemove = false,
+      bool isAdd = false,
+      bool isEqual = false,
+      bool isSubstract = false}) {
+    String ret = widget.moneyController.text;
+    if (isAdd || isSubstract) {
+      if (ret.isEmpty) {
+        ret = "0";
+      } else if (ret.characters.last != '+' && ret.characters.last != '-') {
+        ret = ret + (isAdd ? "+" : "-");
+        symbolCounter++;
+      }
+      if (widget.callback != null) {
+        widget.callback!(ret);
+      }
+      return;
+    }
+    if (isEqual) {
+      if (ret.isEmpty) {
+        ret = "0";
+      } else if (symbolCounter != 0) {
+        // analyze the expression and calculate the result
+        // remove tailing '+' or '-'
+        if (ret.characters.last == '+' || ret.characters.last == '-') {
+          ret = ret.substring(0, ret.length - 1);
+          symbolCounter--;
+        }
+        var parts = ret.split(RegExp(r'[+|-]'));
+        var result = double.parse(parts.first);
+        int currentIndex = parts[0].length;
+        for (var i = 1; i < parts.length; i++) {
+          if (currentIndex < ret.length) {
+            if (ret[currentIndex] == '+') {
+              result += double.parse(parts[i]);
+            } else if (ret[currentIndex] == '-') {
+              result -= double.parse(parts[i]);
+            }
+            currentIndex = currentIndex + parts[i].length + 1;
+          }
+        }
+        ret = result.toString();
+        symbolCounter = 0;
+      }
+      if (widget.callback != null) {
+        widget.callback!(ret);
+      }
+      return;
+    }
     if (isRemove) {
+      if (ret[ret.length - 1] == '+' || ret[ret.length - 1] == '-') {
+        symbolCounter--;
+      }
       ret = ret.substring(0, ret.length - 1);
     } else if (isDot) {
       ret = ret + ".";
@@ -23,9 +81,20 @@ class MoneyNumberTablet extends StatelessWidget {
     // ret = ret.replaceFirst(RegExp('^0+'), '');
     // make sure only two number after dot
     // use '[0-9]+' to ensure that if tmp is Empty then set to "0"
-    ret = RegExp("[0-9]+[.]?[0-9]{0,2}").stringMatch(ret) ?? "0";
-    if (callback != null) {
-      callback!(ret);
+    var numberPattern = RegExp("[0-9]+[.]?[0-9]{0,2}");
+    if (symbolCounter == 0) {
+      ret = RegExp("[0-9]+[.]?[0-9]{0,2}").stringMatch(ret) ?? "0";
+    } else {
+      var parts = ret.split(RegExp(r'[+|-]'));
+      var matched = numberPattern.stringMatch(parts.last);
+      if (matched == null ||
+          matched.isEmpty ||
+          matched.length != parts.last.length) {
+        ret = ret.substring(0, ret.length - 1);
+      }
+    }
+    if (widget.callback != null) {
+      widget.callback!(ret);
     }
   }
 
@@ -36,14 +105,15 @@ class MoneyNumberTablet extends StatelessWidget {
       shrinkWrap: true,
       primary: false,
       physics: const NeverScrollableScrollPhysics(),
-      crossAxisCount: 3,
+      crossAxisCount: 4,
       childAspectRatio: 4 / 2.5,
       mainAxisSpacing: 4.0,
       crossAxisSpacing: 4.0,
       children: <Widget>[
         OutlinedButton(
           style: OutlinedButton.styleFrom(
-            foregroundColor: Colors.black, textStyle: const TextStyle(fontSize: 18),
+            foregroundColor: Colors.black,
+            textStyle: const TextStyle(fontSize: 18),
             backgroundColor: Colors.white,
           ),
           child: const Text('1'),
@@ -53,7 +123,8 @@ class MoneyNumberTablet extends StatelessWidget {
         ),
         OutlinedButton(
           style: OutlinedButton.styleFrom(
-            foregroundColor: Colors.black, textStyle: const TextStyle(fontSize: 18),
+            foregroundColor: Colors.black,
+            textStyle: const TextStyle(fontSize: 18),
             backgroundColor: Colors.white,
           ),
           child: const Text('2'),
@@ -63,7 +134,8 @@ class MoneyNumberTablet extends StatelessWidget {
         ),
         OutlinedButton(
           style: OutlinedButton.styleFrom(
-            foregroundColor: Colors.black, textStyle: const TextStyle(fontSize: 18),
+            foregroundColor: Colors.black,
+            textStyle: const TextStyle(fontSize: 18),
             backgroundColor: Colors.white,
           ),
           child: const Text('3'),
@@ -73,79 +145,8 @@ class MoneyNumberTablet extends StatelessWidget {
         ),
         OutlinedButton(
             style: OutlinedButton.styleFrom(
-              foregroundColor: Colors.black, textStyle: const TextStyle(fontSize: 18),
-              backgroundColor: Colors.white,
-            ),
-            child: const Text('4'),
-            onPressed: () {
-              _onNumberTabletPressed(number: 4);
-            }),
-        OutlinedButton(
-            style: OutlinedButton.styleFrom(
-              foregroundColor: Colors.black, textStyle: const TextStyle(fontSize: 18),
-              backgroundColor: Colors.white,
-            ),
-            child: const Text('5'),
-            onPressed: () {
-              _onNumberTabletPressed(number: 5);
-            }),
-        OutlinedButton(
-            style: OutlinedButton.styleFrom(
-              foregroundColor: Colors.black, textStyle: const TextStyle(fontSize: 18),
-              backgroundColor: Colors.white,
-            ),
-            child: const Text('6'),
-            onPressed: () {
-              _onNumberTabletPressed(number: 6);
-            }),
-        OutlinedButton(
-            style: OutlinedButton.styleFrom(
-              foregroundColor: Colors.black, textStyle: const TextStyle(fontSize: 18),
-              backgroundColor: Colors.white,
-            ),
-            child: const Text('7'),
-            onPressed: () {
-              _onNumberTabletPressed(number: 7);
-            }),
-        OutlinedButton(
-            style: OutlinedButton.styleFrom(
-              foregroundColor: Colors.black, textStyle: const TextStyle(fontSize: 18),
-              backgroundColor: Colors.white,
-            ),
-            child: const Text('8'),
-            onPressed: () {
-              _onNumberTabletPressed(number: 8);
-            }),
-        OutlinedButton(
-            style: OutlinedButton.styleFrom(
-              foregroundColor: Colors.black, textStyle: const TextStyle(fontSize: 18),
-              backgroundColor: Colors.white,
-            ),
-            child: const Text('9'),
-            onPressed: () {
-              _onNumberTabletPressed(number: 9);
-            }),
-        OutlinedButton(
-            style: OutlinedButton.styleFrom(
-              foregroundColor: Colors.black, textStyle: const TextStyle(fontSize: 18),
-              backgroundColor: Colors.white,
-            ),
-            child: const Text('.'),
-            onPressed: () {
-              _onNumberTabletPressed(isDot: true);
-            }),
-        OutlinedButton(
-            style: OutlinedButton.styleFrom(
-              foregroundColor: Colors.black, textStyle: const TextStyle(fontSize: 18),
-              backgroundColor: Colors.white,
-            ),
-            child: const Text('0'),
-            onPressed: () {
-              _onNumberTabletPressed(number: 0);
-            }),
-        OutlinedButton(
-            style: OutlinedButton.styleFrom(
-              foregroundColor: Colors.black, textStyle: const TextStyle(fontSize: 18),
+              foregroundColor: Colors.black,
+              textStyle: const TextStyle(fontSize: 18),
               backgroundColor: Colors.white,
             ),
             child: const Icon(
@@ -155,6 +156,127 @@ class MoneyNumberTablet extends StatelessWidget {
             onPressed: () {
               _onNumberTabletPressed(isRemove: true);
             }),
+        OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.black,
+              textStyle: const TextStyle(fontSize: 18),
+              backgroundColor: Colors.white,
+            ),
+            child: const Text('4'),
+            onPressed: () {
+              _onNumberTabletPressed(number: 4);
+            }),
+        OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.black,
+              textStyle: const TextStyle(fontSize: 18),
+              backgroundColor: Colors.white,
+            ),
+            child: const Text('5'),
+            onPressed: () {
+              _onNumberTabletPressed(number: 5);
+            }),
+        OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.black,
+              textStyle: const TextStyle(fontSize: 18),
+              backgroundColor: Colors.white,
+            ),
+            child: const Text('6'),
+            onPressed: () {
+              _onNumberTabletPressed(number: 6);
+            }),
+        OutlinedButton(
+          style: OutlinedButton.styleFrom(
+            foregroundColor: Colors.black,
+            textStyle: const TextStyle(fontSize: 18),
+            backgroundColor: Colors.white,
+          ),
+          child: const Text('+'),
+          onPressed: () {
+            _onNumberTabletPressed(isAdd: true);
+          },
+        ),
+        OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.black,
+              textStyle: const TextStyle(fontSize: 18),
+              backgroundColor: Colors.white,
+            ),
+            child: const Text('7'),
+            onPressed: () {
+              _onNumberTabletPressed(number: 7);
+            }),
+        OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.black,
+              textStyle: const TextStyle(fontSize: 18),
+              backgroundColor: Colors.white,
+            ),
+            child: const Text('8'),
+            onPressed: () {
+              _onNumberTabletPressed(number: 8);
+            }),
+        OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.black,
+              textStyle: const TextStyle(fontSize: 18),
+              backgroundColor: Colors.white,
+            ),
+            child: const Text('9'),
+            onPressed: () {
+              _onNumberTabletPressed(number: 9);
+            }),
+        OutlinedButton(
+          style: OutlinedButton.styleFrom(
+            foregroundColor: Colors.black,
+            textStyle: const TextStyle(fontSize: 18),
+            backgroundColor: Colors.white,
+          ),
+          child: const Text('-'),
+          onPressed: () {
+            _onNumberTabletPressed(isSubstract: true);
+          },
+        ),
+        OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.black,
+              textStyle: const TextStyle(fontSize: 18),
+              backgroundColor: Colors.white,
+            ),
+            child: const Text('.'),
+            onPressed: () {
+              _onNumberTabletPressed(isDot: true);
+            }),
+        OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.black,
+              textStyle: const TextStyle(fontSize: 18),
+              backgroundColor: Colors.white,
+            ),
+            child: const Text('0'),
+            onPressed: () {
+              _onNumberTabletPressed(number: 0);
+            }),
+        OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.black,
+              textStyle: const TextStyle(fontSize: 18),
+              backgroundColor: Colors.white,
+            ),
+            child: const Text(''),
+            onPressed: null),
+        OutlinedButton(
+          style: OutlinedButton.styleFrom(
+            foregroundColor: Colors.black,
+            textStyle: const TextStyle(fontSize: 18),
+            backgroundColor: Colors.white,
+          ),
+          child: const Text('='),
+          onPressed: () {
+            _onNumberTabletPressed(isEqual: true);
+          },
+        ),
       ],
     );
   }
